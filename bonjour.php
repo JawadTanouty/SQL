e56301198a08ba1c7a397a36d405847660b39425
JawadTanouty <jawad.tanouty@gmail.com>
2025-07-22 14:26:56 +0200

    supplément age

--- bonjour.php
+++ bonjour.php
@@ -19,6 +19,21 @@ if(isset($_GET['couleur'])){
         $couleur = $couleurs_valides[$choix];
 }
 }
+
+$message = "";
+var_dump($_GET);
+
+if (isset($_GET['age'])){
+    $age = (int) $_GET['age']; //Convertir en entier
+
+    if($age >= 18) {
+        $message = "Vous êtes majeur";
+    }else{
+        $message = "Vous êtes mineur";
+    }
+}
+
+    echo $message;
 ?>
 
 
@@ -32,15 +47,22 @@ if(isset($_GET['couleur'])){
 </head>
 <body style="background-color: <?=$couleur ?>; text-align : center; padding-top: 50px;">
     <a href="bonjour.php?nom=Alice">Voir profil</a>
-    
+<br>
     <a href="bonjour.php?couleur=rouge">Rouge</a>
     <a href="bonjour.php?couleur=vert">Vert</a>
     <a href="bonjour.php?couleur=bleu">Bleu</a>
 
     <p>Couleur actuelle : <strong><?= htmlspecialchars($choix ?? 'aucune') ?> </strong> </p>
 
+
+<form action="" method="get">
+        <input type="number" name="age" placeholder="Entrez votre âge">
+        <button type="submit">Envoyer</button>
+</form>
+
 </body>
 </html>
+
 
 
 
