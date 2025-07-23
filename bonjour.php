3ed87f8221a7aadfff988937a396d3f0e6cd4246
JawadTanouty <jawad.tanouty@gmail.com>
2025-07-23 09:14:33 +0200

    blabla

--- bonjour.php
+++ bonjour.php
@@ -2,18 +2,18 @@
 
 
 //Vérifie si le paramètre "nom" existe dans l'URL
-if(isset($_GET['nom']) && !empty ($_GET['nom'])) {
+if(isset($_GET["nom"]) && !empty ($_GET["nom"])) {
     //On récup le nom et on le sécurise contre les attaques
-    $nom= htmlspecialchars($_GET['nom']);
+    $nom= htmlspecialchars($_GET["nom"]);
     echo "Bonjour " . $nom;
 } 
 
 
 $couleur = "white";
 
-if(isset($_GET['couleur'])){
-    $choix = $_GET['couleur'];
-    $couleurs_valides= ['rouge' => 'red', 'vert' => 'green', 'bleu' => 'blue'];
+if(isset($_GET["couleur"])){
+    $choix = $_GET["couleur"];
+    $couleurs_valides= ["rouge" => "red", "vert" => "green", "bleu" => "blue"];
 
     if (array_key_exists($choix,$couleurs_valides)) {
         $couleur = $couleurs_valides[$choix];
@@ -22,8 +22,8 @@ if(isset($_GET['couleur'])){
 
 $message = "";
 
-if (isset($_GET['age'])){
-    $age = (int) $_GET['age']; //Convertir en entier
+if (isset($_GET["age"])){
+    $age = (int) $_GET["age"]; //Convertir en entier
 
     if($age >= 18) {
         $message = "Vous êtes majeur";
